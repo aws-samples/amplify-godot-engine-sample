67cebe19ab0bfbd45062aca087dfddf76680a439
Pat Santora <psantora@amazon.com>
2025-03-14 15:05:13 -0700

    feat: add secrets for cross acount roles

--- amplify/functions/gen-ai-chat/handler.ts
+++ amplify/functions/gen-ai-chat/handler.ts
@@ -9,9 +9,8 @@ const stsClient = new STSClient({ region: "us-east-1" });
 // Improve typing for getCredentials
 async function getCredentials(): Promise<AwsCredentialIdentity> {
   const params = {
-    // RoleArn: process.env.CROSS_ACCOUNT_ROLE_ARN,
-    RoleArn: "arn:aws:iam::495599745041:role/GenAIHelperAgent-Sandbox",
-    RoleSessionName: "BedrockAgentCrossAccountSession",
+    RoleArn: env.CROSS_ACCOUNT_ROLE_ARN,
+    RoleSessionName: env.ROLE_SESSION_NAME || 'BedrockAgentCrossAccountSession',
     DurationSeconds: 900
   };
 
@@ -114,8 +113,8 @@ export const handler: Handler = async (event) => {
  
     const response = await bedrockAgentClient.send(
       new InvokeAgentCommand({
-        agentId: env.AGENT_ID || "~TEHSL4B2MV",
-        agentAliasId: env.AGENT_ALIAS_ID || "~DNPQV4JTP4",
+        agentId: env.AGENT_ID,
+        agentAliasId: env.AGENT_ALIAS_ID,
         sessionId: event.arguments.sessionId || "default-session",
         inputText: prompt
       })
