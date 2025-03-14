2ae4332d22668f8dec611400496cff27860d0b7e
Frederic Nowak <fredenow@amazon.com>
2025-03-14 09:46:24 +0100

    remove storage bucket permissions to amplify service

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,8 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { storage } from './storage/resource';
-import * as iam from "aws-cdk-lib/aws-iam"
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 const backend = defineBackend({
     storage: storage
 });
-backend.storage.resources.bucket.grantWrite(new iam.ServicePrincipal("amplify.amazonaws.com"))
